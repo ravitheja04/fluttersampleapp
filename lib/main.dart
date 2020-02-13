26f582f0e98ac7160e108c10d8d29d46bd5aa421
raviteja04 <r.theja04@gmail.com>
2020-02-13 18:54:33 +0530

    sample appp

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+const BASE_URL = $BASE_URL_DEV;
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
